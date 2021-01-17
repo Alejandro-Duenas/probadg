d280e7638f7f76696e7fa309fc8afa910d3abdb4
Alejandro-Duenas-Garcia <alejandro.duenas@davivienda.com>
2021-01-16 21:37:53 -0500

    feat: organise the setup of the package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='my_probability',
+      version = '0.1',
+      description = 'Binomial and Gaussian objects',
+      packages = ['my_probability'],
+      zip_safe = False)
